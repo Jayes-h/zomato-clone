f91fc7ba56f9043770fddf1ae70ac84d624ebf68
Jayesh Patil <jayesh.patil0627@gmail.com>
2024-08-12 13:27:21 +0530

    first code push

---
+++ myscript.js
@@ -0,0 +1,35 @@
+$('.slider01').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
+
+$('.slider02').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  })
+  
